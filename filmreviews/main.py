ad917a1aa799923a771ab3a3f5966a73d11a5f90
chx56 <russoantonio451@gmail.com>
2022-05-09 17:44:18 +0200

    bho

---
+++ filmreviews/main.py
@@ -0,0 +1,11 @@
+from whoosh.filedb.filestore import Storage, FileStorage
+from whoosh.index import Index
+from whoosh.qparser import syntax, Plugin, QueryParser, MultifieldPlugin
+import re
+
+from whoosh.searching import Searcher
+
+
+
+
+
