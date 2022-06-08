3f1677e23e23cb51e839c4bc4215b7c2d8f8dda9
chx56 <russoantonio451@gmail.com>
2022-06-08 21:28:39 +0200

    update

--- filmreviews/movie_search.py
+++ filmreviews/movie_search.py
@@ -4,15 +4,28 @@ import time
 
 url = "https://advanced-movie-search.p.rapidapi.com/genre/movie/list"
 
+ #Qui ho vari headers per le richieste get delle API vito che quelle gratuite sono limitate a 500 al mese
+
+# headers = {
+# 	"X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
+# 	"X-RapidAPI-Key": "2f578134c9msha2d0e92b52a1562p114a96jsn29f99d2ca53b"
+# }
+
+# headers_2 = {
+#     'X-RapidAPI-Host': 'advanced-movie-search.p.rapidapi.com',
+#     'X-RapidAPI-Key': '4581bee7e0msh00a097b3e544861p17d736jsna6aeef75fb56'
+# }
+
+# headers = {
+# 	"X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
+# 	"X-RapidAPI-Key": "5a0d0d71a7msh325031a0ddf7fc8p1ee1e2jsn0efd03be1707"
+# }
+
 headers = {
 	"X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
-	"X-RapidAPI-Key": "2f578134c9msha2d0e92b52a1562p114a96jsn29f99d2ca53b"
+	"X-RapidAPI-Key": "59124b1fbdmsh7cb07020705c585p11f13ejsn635ddd484fd0"
 }
 
-headers_2 = {
-    'X-RapidAPI-Host': 'advanced-movie-search.p.rapidapi.com',
-    'X-RapidAPI-Key': '4581bee7e0msh00a097b3e544861p17d736jsna6aeef75fb56'
-}
 
 class movie_search:
     base_url = 'https://advanced-movie-search.p.rapidapi.com/'
@@ -20,10 +33,13 @@ class movie_search:
     url_search = 'https://advanced-movie-search.p.rapidapi.com/discover/movie'
     path_index = './index/index.json'
 
+
     def getGenre(self):
         self.genr = requests.request("GET", self.url_genre, headers=headers).text
         return self.genr
 
+
+    # Attraverso le API ottengo un json con 20 film
     def getSinglePageMovie(self,querystring):
 
         r = requests.get(self.url_search, headers=headers, params=querystring)
@@ -35,13 +51,16 @@ class movie_search:
         
         return r.json()
 
-    def getGenrId(self):
+    #attraverso una richiesta alle API crea una lista con i codici dei vari generi che dovranno essere usate per la ricerca dei film
+    def getGenrId(self): 
         genre = json.loads(self.getGenre())
         self.genreId = []
         for i in range(len(genre["genres"])):
             self.genreId.append(genre["genres"][i]["id"])
         return self.genreId
 
+
+    #Effettuo più richieste per ottenere più pagine (ogni pagina contiene 20 film)
     def getAllPageMovie(self):
         time.sleep(1)
         movies = {'movies':[]}
@@ -55,7 +74,7 @@ class movie_search:
                 if(resp == {'message':'exit'}):
                     flag = False
                 elif(resp == {'message':'wait'}):
-
+                    wait_flag = True
                     jsonString = json.dumps(movies)
                     jsonFile = open(self.path_index, "w")
                     jsonFile.write(jsonString)
@@ -64,7 +83,11 @@ class movie_search:
                     time.sleep(5)
                 else:
                     for i in range(19):
-                        obj = {'id':resp["results"][i]["id"],'title':resp["results"][i]["title"],'overview':resp["results"][i]["overview"]}
+                        try:
+                            obj = {'id':resp["results"][i]["id"],'title':resp["results"][i]["title"],'release_date':resp["results"][i]["release_date"].split('-')[0],'overview':resp["results"][i]["overview"]}
+                        except KeyError:
+                            obj = {'id':resp["results"][i]["id"],'title':resp["results"][i]["title"],'release_date':'0000','overview':resp["results"][i]["overview"]}
+       
                         movies["movies"].append(obj)                   
                 count += 1
                 time.sleep(1)
@@ -76,16 +99,12 @@ class movie_search:
     def readIndex(self):
         data = open(self.path_index)
         movies = json.load(data)
-        
-        for i in range(len(movies["movies"])):
-            pass
-            #print(movies["movies"][i]["title"])
-
         #print(len(movies["movies"]))
         return movies
 
 #print(response.text)
 #test = movie_search()
+#test.getGenre()
 #print(test.getGenrId())
 #time.sleep(1)
 #print(test.getSinglePageMovie({'with_genres': 80, 'page': 1}).json()["results"][0]["title"])
