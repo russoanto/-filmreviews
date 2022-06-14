fa9984ff0e623dde1b03b65da38d9e6837981ce1
FlEtOsAuRoM99 <mariotti.samuele1999@gmail.com>
2022-06-14 22:29:44 +0200

    now we have a new class for imdb

--- filmreviews/imdb/imdbClass.py
+++ filmreviews/imdb/imdbClass.py
@@ -2,45 +2,75 @@ import os
 from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 from concurrent import futures
-import json
 import requests
-import urllib3
+import movie_search
 
- 
-'''
 class imdb:
-    def __init__():
+
+    def __init__(self, path_index, data, url):
+        self.url = url
+        self.path_index = path_index
+        self._MOVIES = []
+        self.films = []
+        for i in range(len(data["movies"])):
+            self.films.append({'id':data["movies"][i]["id"],'title':data["movies"][i]["title"],'date':data["movies"][i]["release_date"]})
+        
+    @staticmethod
+    def format_name(name):
+        film_name = name.lower()
+        #film_name = film_name.replace(' ','+')
+        film_name = film_name.replace('\'','')
+        film_name = film_name.replace('-', '_')
+       # film_name = film_name.replace(',','')
+       # film_name = film_name.replace(':','')
+        return str(film_name)
+
+    def get_movie_info(self, film_name, date):
+        return [film_name, date]
+
+    def scrapingDowload(self, name, url):
+        self.url += '/find?q=' #inserisco il link corretto.
+
+        self.url += name + '&title_type=feature&sort=year,asc'
+            #'&title_type=feature&release_date=1986-01-01,&sort=year,asc&view=advanced'
+        print(self.url)
+        soup = BeautifulSoup(requests.get(self.url).content, 'html.parser') 
+
+        for i in soup.find_all('a' ):
+            if(i.get_text().lower() == str(name).lower()):
+                print(i.get('href'))
+                break
+        try:
+            self.url = 'https://www.imdb.com' + i.get('href') + 'reviews?sort=userRating&dir=desc&ratingFilter=0'
+            soup = BeautifulSoup(requests.get(self.url).content, 'html.parser') 
+            count=0
+            print(self.url)
+            for i in soup.find_all('div', class_='text show-more__control'):
+                print(i.get_text(),'\n')
+                count +=1
+            print(count)
+        except:
+            print("è successo")
+        self.resetUrl(url)
+
+    def resetUrl(self, url):
+        self.url = url
+
+
+searcher = movie_search.movie_search("../index/index.json")
+data = searcher.readIndex()
+test = imdb("/movies/index.json", data,  "https://www.imdb.com/")
+resp = []
+for i in range(len(data["movies"])):
+    resp.append(test.get_movie_info(test.format_name(data["movies"][i]["title"]),data["movies"][i]["release_date"]))
+for i in range(len(data["movies"])):
+    print(resp[i][0])
+for i in range(len(data["movies"])):
+    test.scrapingDowload(resp[i][0], "https://www.imdb.com/")
+
+#test.file_json()
+
 '''
-
-
-def scrapingDowload(name, year = ''):
-    newname = name
-    url = 'https://www.imdb.com/find?q='
-    url += name + '&title_type=feature&sort=year,asc'
-        #'&title_type=feature&release_date=1986-01-01,&sort=year,asc&view=advanced'
-    soup = BeautifulSoup(requests.get(url).content, 'html.parser') 
-    for i in soup.find_all('a' ):
-        #print(i.get_text())
-        if(i.get_text().lower() == str(name).lower()):
-            print(i.get('href'))
-            break
-    try:
-        url = 'https://www.imdb.com' + i.get('href') + 'reviews?sort=userRating&dir=desc&ratingFilter=0'
-        soup = BeautifulSoup(requests.get(url).content, 'html.parser') 
-        count=0
-        print(url)
-        for i in soup.find_all('div', class_='text show-more__control'):
-            print(i.get_text(),'\n')
-            count +=1
-        print(count)
-    except:
-        print("è successo")
-
-# with open(test.path_index) as json_file:
-#     data = json.load(json_file)
-#     for i in range(test.num_film):
-#          print(test.movie_info(data[str(i)]["name"]))
-with open("../index/final_index.json") as json_file:
-    data = json.load(json_file)
-    for i in range(0, 43899):
-        scrapingDowload(data[str(i)]["name"])
+for i in range(0, 9440):
+    test.scrapingDowload(fileJson[i]["title"])
+''' 
