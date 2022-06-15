a62956a6b7bd7a8a177818d2fcf181cb6704dd8d
FlEtOsAuRoM99 <mariotti.samuele1999@gmail.com>
2022-06-15 16:13:52 +0200

    add language for request to imdb

--- filmreviews/imdb/imdbClass.py
+++ filmreviews/imdb/imdbClass.py
@@ -18,7 +18,7 @@ class imdb:
     @staticmethod
     def format_name(name):
         film_name = name.lower()
-        #film_name = film_name.replace(' ','+')
+       # film_name = film_name.replace(' ','+')
         film_name = film_name.replace('\'','')
         film_name = film_name.replace('-', '_')
        # film_name = film_name.replace(',','')
@@ -28,21 +28,23 @@ class imdb:
     def get_movie_info(self, film_name, date):
         return [film_name, date]
 
-    def scrapingDowload(self, name, url):
+    def scrapingDownload(self, name, url):
         self.url += '/find?q=' #inserisco il link corretto.
+        headers = {"Accept-Language": "en-US,en;q=0.5"}
+        name = 'sonic the hedgehog 2'
 
         self.url += name + '&title_type=feature&sort=year,asc'
             #'&title_type=feature&release_date=1986-01-01,&sort=year,asc&view=advanced'
         print(self.url)
-        soup = BeautifulSoup(requests.get(self.url).content, 'html.parser') 
-
-        for i in soup.find_all('a' ):
-            if(i.get_text().lower() == str(name).lower()):
+        soup = BeautifulSoup(requests.get(self.url, headers=headers).content, 'html.parser') 
+        for i in soup.find_all('a'):
+           # print(i.get_text().lower().lstrip().rstrip(), " ", str(name).lower())
+            if(i.get_text().lower().strip()== str(name)):
                 print(i.get('href'))
                 break
         try:
             self.url = 'https://www.imdb.com' + i.get('href') + 'reviews?sort=userRating&dir=desc&ratingFilter=0'
-            soup = BeautifulSoup(requests.get(self.url).content, 'html.parser') 
+            soup = BeautifulSoup(requests.get(self.url, headers=headers).content, 'html.parser') 
             count=0
             print(self.url)
             for i in soup.find_all('div', class_='text show-more__control'):
@@ -59,19 +61,14 @@ class imdb:
 
 searcher = movie_search.movie_search()
 data = searcher.readIndex()
-<<<<<<< HEAD
 test = imdb("/movies/index.json", data,  "https://www.imdb.com")
-=======
-
-test = imdb("/movies/index.json", data,  "https://www.imdb.com/")
->>>>>>> 12b917d68da6682bde695072a4b555523ccc8c6c
 resp = []
 for i in range(len(data["movies"])):
     resp.append(test.get_movie_info(test.format_name(data["movies"][i]["title"]),data["movies"][i]["release_date"]))
 for i in range(len(data["movies"])):
-    print(resp[i][0])
+    print(resp[i])
 for i in range(len(data["movies"])):
-    test.scrapingDowload(resp[i][0], "https://www.imdb.com")
+    test.scrapingDownload(resp[i][0], "https://www.imdb.com")
 
 #test.file_json()
 
