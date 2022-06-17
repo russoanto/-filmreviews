29e8e6d42a37e967155db69baa6d4428c84ac2e0
pesto131 <ricola1304@gmail.com>
2022-06-17 13:04:17 +0200

    abbiamo tutte le info

--- filmreviews/imdb/imdbClass.py
+++ filmreviews/imdb/imdbClass.py
@@ -5,6 +5,10 @@ from concurrent import futures
 import requests
 import movie_search
 
+
+
+
+
 class imdb:
 
     def __init__(self, path_index, data, url):
@@ -34,22 +38,47 @@ class imdb:
 
         self.url += name + '&title_type=feature&sort=year,asc'
             #'&title_type=feature&release_date=1986-01-01,&sort=year,asc&view=advanced'
-        print(self.url)
+        #print(self.url)
+
+
         soup = BeautifulSoup(requests.get(self.url, headers=headers).content, 'html.parser') 
+
+        
+
         for i in soup.find_all('a'):
            # print(i.get_text().lower().lstrip().rstrip(), " ", str(name).lower())
             if(i.get_text().lower().strip()== str(name)):
-                print(i.get('href'))
+                #print(i.get('href'))
+                self.url = 'https://www.imdb.com' + i.get('href') + '?ref_=tt_urv'
                 break
+        print("------------------------------")
+        print(self.url)
+
+        soup = BeautifulSoup(requests.get(self.url, headers=headers).content, 'html.parser') 
+
+        ###################################
+        #qua chiamo tutte le funzioni
+        try:
+            self.get_director_writers_starts(soup)
+            self.get_duration(soup)
+            self.get_genres(soup)
+            self.get_rating(soup)
+        except:
+            f.write(str(self.url))
+            f.write('\n')
+
+
+        ################################
+
         try:
             self.url = 'https://www.imdb.com' + i.get('href') + 'reviews?sort=userRating&dir=desc&ratingFilter=0'
             soup = BeautifulSoup(requests.get(self.url, headers=headers).content, 'html.parser') 
             count=0
             print(self.url)
             for i in soup.find_all('div', class_='text show-more__control'):
-                print(i.get_text(),'\n')
+                #print(i.get_text(),'\n')
                 count +=1
-            print(count)
+            #print(count)
         except:
             print("è successo")
         self.resetUrl(url)
@@ -58,16 +87,62 @@ class imdb:
         self.url = url
 
 
+    #########################
+
+    def get_rating(self, soup):
+        
+        for span in soup.find_all('span', class_='sc-7ab21ed2-1 jGRxWM'):
+            print(span.text)   
+            break
+
+
+    def get_genres(self, soup):
+        for div in soup.find_all('div', class_='ipc-chip-list sc-16ede01-4 bMBIRz'):
+            for a in div.contents:
+                print(a.text)    
+            break
+
+    def get_duration(self, soup):
+        for ul in soup.find_all('ul', class_='ipc-inline-list ipc-inline-list--show-dividers sc-8c396aa2-0 kqWovI baseAlt'):
+            print(ul.contents[2].text)
+            break
+
+    
+    #ho fatto cosi perchè hanno tutti la stessa classe, sono dovuto scendere ai vari livelli partendo dalla classe di partenza
+    def get_director_writers_starts(self, soup):
+        for ul in soup.find_all('ul', class_='ipc-metadata-list ipc-metadata-list--dividers-all title-pc-list ipc-metadata-list--baseAlt'):
+            #print(i.get_text(),'\n')
+            lilist = ul.contents
+            
+            #director
+            print(lilist[0].contents[1].text)
+            
+            #writers
+            for actors in lilist[1].contents[1].contents[0]:
+                #print(act.prettify())
+                print(actors.contents[0].contents[0].text)
+                
+            #stars
+            for stars in lilist[2].contents[1].contents[0]:
+                #print(stars.prettify())
+                print(stars.contents[0].contents[0].text)
+                
+            
+            break
+            
+f = open("demofile2.txt", "a")
+
+
 searcher = movie_search.movie_search()
 data = searcher.readIndex()
 test = imdb("/movies/index.json", data,  "https://www.imdb.com")
 resp = []
 for i in range(len(data["movies"])):
     resp.append(test.get_movie_info(test.format_name(data["movies"][i]["title"]),data["movies"][i]["release_date"]))
-for i in range(len(data["movies"])):
-    print(resp[i])
-for i in range(len(data["movies"])):
+    #print(resp[i])
     test.scrapingDownload(resp[i][0], "https://www.imdb.com")
+
+f.close()
 
 #test.file_json()
 
