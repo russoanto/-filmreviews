bd1f8334f42477970979ca4a26bab0ea95bfa217
FlEtOsAuRoM99 <mariotti.samuele1999@gmail.com>
2022-06-21 23:15:35 +0200

    rename test.txt in informations.txt and i semplify a code in main.py with functions

--- filmreviews/main.py
+++ filmreviews/main.py
@@ -44,14 +44,13 @@ def compute_discounted_cumulative_gain(data):
         return 0
     return data[0] + sum([(data[i] / math.log(i + 1, 2)) for i in range(1, len(data))])
 
-def getInformation_indexing(imdb, pomodoro):
+def getInformation_indexing(get):
     '''
-    Fa scraping e indexing delle due fonti, imdb e pomodoro.
+    Fa scraping e indexing delle due fonti, imdb e pomodoro (get riceve uno dei due alla volta).
     '''
-    imdb.get_all_information_t()
-    imdb.indexing()
-    pomodoro.scrape_all_information()
-    pomodoro.indexing()
+    print("entro, ", type(get))
+    get.get_all_information_t()
+    get.indexing()
 
 def printInformation(result):
     '''
@@ -62,6 +61,38 @@ def printInformation(result):
         for x in result:
             print(x["title"] + ' --> ' + str(x.score)+ '\n')
             scores.append(x.score)
+
+def searchInIndex(queryPom, queryImd, searchPOM, searchIMD):
+    # #--------------------------------------------------------
+
+    # im = QueryParser(None, imdb.ix.schema, group=syntax.OrGroup)
+    # fieldboosts = {
+    #         'title': 6,
+    #         'content':1,
+    # }
+    # mfp = MultifieldPlugin(('title', 'content', 'casts','directors'), fieldboosts=fieldboosts)
+    # im.add_plugin(mfp)
+
+
+    # p.add_plugin(FieldBoosterPlugin({
+    #         'title':40,'content':40,'release_date':40,'genres':40,'directors':40,'casts':40,
+    # }))
+    print(queryImd, " ", queryPom)
+    searchers = [(searchPOM,'tomato'), (searchIMD,'imdb')]
+    top_k = merge_search.aggregate_search(queryPom, searchers, 5)
+
+    with open('./informations.txt', 'w') as openfile:
+        for i in top_k:
+            for j in i:
+                openfile.write(str(j)+ ' ')
+            openfile.write('\n')
+
+    resultsIMD = searchIMD.search(queryImd,terms=True, limit=20)
+    resultsPOM = searchPOM.search(queryPom, terms=True, limit=20)
+
+    printInformation(resultsIMD)
+    printInformation(resultsPOM)
+
 def main():
 
     searcher = movie_search.movie_search()
@@ -71,7 +102,8 @@ def main():
 
     imdb = imdbClass.imdbIndex("./movies/index.json", data, "https://www.imdb.com")
 
-    #getInformation_indexing(imdb, pomodoro)
+    # getInformation_indexing(imdb)
+    # getInformation_indexing(pomodoro)
     
     searchPOM = pomodoro.ix.searcher()
     searchIMD = imdb.ix.searcher()
@@ -88,44 +120,15 @@ def main():
     p.add_plugin(FieldBoosterPlugin({
             'title':40, 'casts':40, 'release_date':40,'genres':40,'directors':40,'content':40,
     }))
-    title = input('Inserire il parametro: ')
-    query_txt = re.sub("\s+[1I]$", "", title.strip())
+    
+    inQuery = input('Inserire il parametro: ')
+    query_txt = re.sub("\s+[1I]$", "", inQuery.strip())
 
+    #elaboro query
     queryPom = p.parse(query_txt)
-
-
-# #--------------------------------------------------------
-
-    # im = QueryParser(None, imdb.ix.schema, group=syntax.OrGroup)
-    # fieldboosts = {
-    #         'title': 6,
-    #         'content':1,
-    # }
-    # mfp = MultifieldPlugin(('title', 'content', 'casts','directors'), fieldboosts=fieldboosts)
-    # im.add_plugin(mfp)
-
-
-    # p.add_plugin(FieldBoosterPlugin({
-    #         'title':40,'content':40,'release_date':40,'genres':40,'directors':40,'casts':40,
-    # }))
-
-    queryImd = p.parse(title)
+    queryImd = p.parse(inQuery)
     
-    searchers = [(searchPOM,'tomato'), (searchIMD,'imdb')]
-    top_k = merge_search.aggregate_search(queryPom, searchers, 5)
-
-    with open('./test.txt', 'a') as openfile:
-        for i in top_k:
-            for j in i:
-                openfile.write(str(j)+ ' ')
-            openfile.write('\n')
-
-    resultsIMD = searchIMD.search(queryImd,terms=True, limit=20)
-    resultsPOM = searchPOM.search(queryPom, terms=True, limit=20)
-
-    printInformation(resultsIMD)
-    printInformation(resultsPOM)
-    
+    searchInIndex(queryPom, queryImd, searchPOM, searchIMD) #ricerca negli index richiama anche la funzione printInformation()
 
 
 main()
