3ce6fef37075e0f9035c76c1ec4c28311d9729ea
chx56 <russoantonio451@gmail.com>
2022-06-24 16:47:24 +0200

    sto cambiando tutto

--- filmreviews/main.py
+++ filmreviews/main.py
@@ -3,11 +3,10 @@ from whoosh.qparser import syntax, Plugin, QueryParser, MultifieldPlugin
 import re
 from whoosh.index import open_dir
 import tqdm
-from benchmark import parse_suite
-from benchmark import BenchmarkSuite,BenchmarkResult,BenchmarkEntry
+from setup_benchmark import parse_suite
+from setup_benchmark import BenckmarkList,BenchmarkResult,RelevantDocument
 import argparse
 import math
-
 
 
 RELEVANCE_THRESHOLD = 2
@@ -17,52 +16,36 @@ def niceprint(top_k):
     #ogni elemento di topk è aggregatehit dove ho la lista delle hit e punteggio già ordinato
 
     #attualmente prendo solo u valori più lunghi per fare il marge, ma volevo farvelo vedere cosi mi dite se c'è altro da modificare
-    with open('./file2.txt', 'a') as openfile:
-        for agghit in top_k:
-            #devo mettere assieme
-            #e preparare una lista di risultati dove ho le info
-            #sono già ordinate quindi mi basta fare degli append
-            #trama più lubnga
-            #recensioni metto assieme
-            #durata 1 sola
-            #genres (soluzione bonus)
-            #attori e cast, regista(soluzione bonus)
-            
-            #score = agghit[1]
 
-            hitlist = []
-            #hitlist.clear()
-            for hitstr in agghit[0]:
-                
-                
-                hit = hitstr[0]
+    for agghit in top_k:
+        #devo mettere assieme
+        #e preparare una lista di risultati dove ho le info
+        #sono già ordinate quindi mi basta fare degli append
+        #trama più lubnga
+        #recensioni metto assieme
+        #durata 1 sola
+        #genres (soluzione bonus)
+        #attori e cast, regista(soluzione bonus)
+        
+        #score = agghit[1]
 
-                hitlist.append(hit)
+        hitlist = []
+        #hitlist.clear()
+        for hitstr in agghit[0]:
+            hit = hitstr[0]
+            hitlist.append(hit)
+        
+        a = mergeSameHit(hitlist)
+        
+        param =["id", "title","genres", "content", "directors", "casts", "release_date", "runtime","reviews"]
+        answare = input('vuoi stampare le recensioni per ' + str(a[param[1]]) + '? (si = 1): ')
+        for i in param:
+            if i != "reviews":
+                print(i + ': ' + str(a[i]) + '\n')
+            elif answare == "1":
+                print(i + ': ' + replaceReviews(str(a[i])) + '\n')
 
-            a = mergeSameHit(hitlist)
-            
-            param =["id", "title","genres", "content", "directors", "casts", "release_date", "runtime","reviews"]
-            answare = input('vuoi stampare le recensioni per ' + str(a["title"]) + '? (si = 1): ')
-            for i in param:
-                if i != "reviews":
-                    print(i + ': ' + str(a[i]) + '\n')
-                elif answare == "1":
-                    print(i + ': ' + replaceReviews(str(a[i])) + '\n')
-
-
-            print("----------------------------")
-            # for z in a:
-                
-            #     if z == 'reviews':
-            #         answare = input('vuoi visualizzare anche i commenti? (1 = si)')
-            #         if answare == 1:
-            #             openfile.write(z + ":\n" + replaceReviews(a[z]))
-            #     else:
-            #         openfile.write(z + ": " + str(a[z]))  
-            #     openfile.write("\n")
-            
-            #openfile.write("---------------------------\n")  
-
+        print("----------------------------")
 
 
 def replaceReviews(s):
@@ -110,20 +93,7 @@ def mergeSameHit(hitlist):
 
 #######################################################
 
-class FieldBoosterPlugin(Plugin):
-        def __init__(self, boosts):
-            self.boosts = boosts
 
-        def filters(self, parser):
-            return [(self.do_boost, 105)]
-
-        def do_boost(self, parser, group):
-            for i, node in enumerate(group):
-                if isinstance(node, syntax.GroupNode):
-                    group[i] = self.do_boost(parser, node)
-                elif node.has_fieldname and node.fieldname is not None:
-                    node.set_boost(node.boost * self.boosts.get(node.fieldname, 1.0))
-            return group
 
 def getInformation_indexing(get):
     '''
@@ -162,73 +132,55 @@ def create_parser(idx):
     p = QueryParser(None, idx.ix.schema, group=syntax.OrGroup)
     fieldboosts = {
             'title': 6,
-            'content':1,
+            'content':2,
     }
+
+    #Returns a QueryParser configured to search in multiple fields. (documentazione whoosh)
+    #Instead of assigning unfielded clauses to a default field, this parser transforms them 
+    # into an OR clause that searches a list of fields. For example, if the list of multi-fields is 
+    # “f1”, “f2” and the query string is “hello there”, the class will parse “(f1:hello OR f2:hello) 
+    # (f1:there OR f2:there)”. This is very useful when you have two textual fields (e.g. “title” and “content”) 
+    # you want to search by default
     mfp = MultifieldPlugin(('title', 'content', 'casts','directors'), fieldboosts=fieldboosts)
     p.add_plugin(mfp)
 
-
-    p.add_plugin(FieldBoosterPlugin({
-            'title':40, 'casts':40, 'release_date':40,'genres':40,'directors':40,'content':40,
-    }))
     return p
 
 
-def searchInIndex(queryPom, queryImd, searchPOM, searchIMD):
-    # #--------------------------------------------------------
-
-    # im = QueryParser(None, imdb.ix.schema, group=syntax.OrGroup)
-    # fieldboosts = {
-    #         'title': 6,
-    #         'content':1,
-    # }
-    # mfp = MultifieldPlugin(('title', 'content', 'casts','directors'), fieldboosts=fieldboosts)
-    # im.add_plugin(mfp)
-
-
-    # p.add_plugin(FieldBoosterPlugin({
-    #         'title':40,'content':40,'release_date':40,'genres':40,'directors':40,'casts':40,
-    # }))
-    print(queryImd, " ", queryPom)
+def searchInIndex(query,searchPOM, searchIMD):
     searchers = [(searchPOM,'tomato'), (searchIMD,'imdb')]
-    top_k = merge_search.aggregate_search(queryPom, searchers, 5)
-
-    with open('./informations.txt', 'w') as openfile:
-        for i in top_k:
-            for j in i:
-                openfile.write(str(j)+ ' ')
-            openfile.write('\n')
-
+    top_k = merge_search.aggregate_search(query, searchers, 5)
     return top_k
 
 def evaluate(suite,pomodoro, imdb):
-    #print(suite.benchmarks)
     res = []
     for bench in suite.benchmarks:
-        print(bench)
         topk = run_query(bench.query,pomodoro,imdb,10)
-        data = {s.id:s.relevance for s in bench.scores}
+        # per ogni query salvo id dei documenti rilevanti con relativo score
+        data = {s.id:s.relevance for s in bench.scores} 
         entries = []
         for row in topk:
             relevance = next((d for hit, source in row.hits if (d := data.get(hit['id'])) is not None), 0)
+            #print(type(relevance))
             entries.append(relevance)
         res.append(BenchmarkResult(bench, entries))
     return res
 
 
-def main(pathBench):
-    parser = argparse.ArgumentParser(description='Benvenuti in filmreviews')
-    parser.set_defaults(action='prompt')
-    subparsers = parser.add_subparsers()
-
-
+def create_command_line(subparsers):
     indexing = subparsers.add_parser('indexing', help='Make web scarping and create whoosh index')
     indexing.set_defaults(action='indexing')
 
-    evaluate_t = subparsers.add_parser('evaluate', help='Evaluate')
-    evaluate_t.add_argument('file', help="The benchmark to run the IR against", type=argparse.FileType('rt'))
-    evaluate_t.set_defaults(action='evaluate')
+    bench_t = subparsers.add_parser('benchmark', help='Benchmark')
+    bench_t.add_argument('file', help="Avvia il processo di benchmark", type=argparse.FileType('r'))
+    bench_t.set_defaults(action='bench')
 
+
+def main():
+    parser = argparse.ArgumentParser(description='Benvenuti in filmreviews')
+    parser.set_defaults(action='prompt')
+    subparsers = parser.add_subparsers()
+    create_command_line(subparsers)
 
     args = parser.parse_args()
 
@@ -252,54 +204,56 @@ def main(pathBench):
             getInformation_indexing(pomodoro)
             getInformation_indexing(imdb)
          
-    elif args.action == 'evaluate':
+    elif args.action == 'bench':
+        #leggo il file di benchmark
         with args.file as fd:
             suite = parse_suite(fd)
-        res = evaluate(suite,pomodoro,imdb)
+        res = evaluate(suite,pomodoro,imdb) #estraggo i per ogni query i documenti rilevanti
+        
         avg_precisions = []
         interp_precisions = [0] * 10
         
         for el in res:
             print(f"{el.query.query} : {[x.relevance for x in el.query.scores]} {el.raw}")
 
-
             # DCG = r1 + r2/log_2(2) + r3/log_3(3) etc ...
             val = BenchmarkResult.compute_discounted_cumulative_gain(el.raw)
            
 
-            # IDEAL DCG
+            # IDEAL DCG (viene fatto)
             ideal_list = sorted([x.relevance for x in el.query.scores], reverse=True)
+            print(ideal_list)
             val_ideal = BenchmarkResult.compute_discounted_cumulative_gain(ideal_list)
             if val_ideal != 0:
                 print(f"DCG: {val}")
                 print(f"IDEAL DCG: {val_ideal}")
                 print(f"NDCG: {val/val_ideal}")
 
-                # NATURAL PRECISION
-                natural_pr = []
-                tot_rel = sum([x.relevance >= RELEVANCE_THRESHOLD for x in el.query.scores])
-                for i, entry in enumerate(el.raw):
-                    if entry >= RELEVANCE_THRESHOLD:
-                        precision = (len(natural_pr) + 1)/(i + 1)
-                        natural_pr.append(precision)
-                print("Natural precision: ")
-                print(" | ".join([f"{(i + 1) / tot_rel}:{value}" for i, value in enumerate(natural_pr)]))
+                # # NATURAL PRECISION
+                # natural_pr = []
+                # tot_rel = sum([x.relevance >= RELEVANCE_THRESHOLD for x in el.query.scores])
+                # for i, entry in enumerate(el.raw):
+                #     if entry >= RELEVANCE_THRESHOLD:
+                #         precision = (len(natural_pr) + 1)/(i + 1)
+                #         natural_pr.append(precision)
+                # print("Natural precision: ")
+                # print(" | ".join([f"{(i + 1) / tot_rel}:{value}" for i, value in enumerate(natural_pr)]))
 
-                # STANDARD PRECISION
-                precisions = [0.0] * 10
-                for i in range(10):
-                    maxval = max([value for j, value in enumerate(natural_pr) if (j + 1) / tot_rel >= (i + 1) / 10], default=0)
-                    precisions[i] = maxval
-                    interp_precisions[i] += maxval
-                print("Standard precision: ")
-                print(" | ".join([f"{(i+1)/10}:{value}" for i, value in enumerate(precisions)]))
+                # # STANDARD PRECISION
+                # precisions = [0.0] * 10
+                # for i in range(10):
+                #     maxval = max([value for j, value in enumerate(natural_pr) if (j + 1) / tot_rel >= (i + 1) / 10], default=0)
+                #     precisions[i] = maxval
+                #     interp_precisions[i] += maxval
+                # print("Standard precision: ")
+                # print(" | ".join([f"{(i+1)/10}:{value}" for i, value in enumerate(precisions)]))
 
-                avg_prc = sum(natural_pr) / tot_rel
-                print(f"Average non-interpolated precision: {avg_prc}")
+                # avg_prc = sum(natural_pr) / tot_rel
+                # print(f"Average non-interpolated precision: {avg_prc}")
 
-                avg_int_prc = sum(precisions) / 10
-                avg_precisions.append(avg_int_prc)
-                print(f"Average interpolated precision: {avg_int_prc}")
+                # avg_int_prc = sum(precisions) / 10
+                # avg_precisions.append(avg_int_prc)
+                # print(f"Average interpolated precision: {avg_int_prc}")
             else:
                 print("Nessun risultato ottenuto")
             print("\n")
@@ -333,22 +287,6 @@ def main(pathBench):
         print("----------------------------")
         print("IMDB")
         printInformation(resultIMD)
-        niceprint(searchInIndex(query, query, searchPOM, searchIMD))
+        niceprint(searchInIndex(query, searchPOM, searchIMD))
 
-
-    # inQuery = input('Inserire il parametro: ')
-        # for i in range(len(queryList)):
-        #     print(queryList[i])
-        #     query_txt = re.sub("\s+[1I]$", "", queryList[i].strip())
-
-        #     #elaboro query
-        #     queryPom = p.parse(query_txt)
-        #     queryImd = p.parse(queryList[i])
-
-        #     searchInIndex(queryPom, queryImd, searchPOM, searchIMD) #ricerca negli index richiama anche la funzione printInformation()
-    
-
-
-# readLineBenchmark("benchmark/query.txt")
-# writeLineBenchmark("benchmark/query.txt", '- title:"Spiderman" OR title:"Iron man"', "hello")
-main("benchmark/query.txt")
+main()
